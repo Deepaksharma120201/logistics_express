66c48fc255a11eb35ca0d0d0086a5dd65334e205
Deepak Sharma <deepaksharma120201@gmail.com>
2024-12-21 16:49:58 +0530

    changes in seach Ride page

--- lib/src/features/screens/customer/user_dashboard/search_ride.dart
+++ lib/src/features/screens/customer/user_dashboard/search_ride.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:logistics_express/src/features/screens/customer/user_dashboard/available_rides.dart';
 import 'package:logistics_express/src/features/subscreens/sidebar/address_filled.dart';
 
 class SearchRide extends StatefulWidget {
@@ -43,25 +42,6 @@ class _SearchRideState extends State<SearchRide> {
               child: const Text('Search Ride'),
             ),
           ],
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Search Ride'),
-        ),
-        backgroundColor: Theme.of(context).cardColor,
-        body: Container(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              AddressFilled(),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Search Ride'),
-              ),
-            ],
-          ),
         ),
       ),
     );
