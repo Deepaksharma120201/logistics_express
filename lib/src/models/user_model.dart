738dd58bc2923b3ed464dd582090945ac49bb588
Shilpa Jindal <shilpajindal2002@gmail.com>
2025-01-25 16:21:12 +0530

    new firebase account add

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -4,6 +4,8 @@ class UserModel {
   final String phoneNo;
   final String password;
   final String email;
+  final String? gender;
+  final String? profileUrl;
 
   const UserModel({
     this.id,
@@ -11,25 +13,33 @@ class UserModel {
     required this.phoneNo,
     required this.password,
     required this.email,
+    this.gender = "",
+    this.profileUrl = "",
   });
 
+  // Convert the model to a Firestore-compatible map
   Map<String, dynamic> toMap() {
     return {
+      "id": id,
       "Name": name,
       "Email": email,
       "Phone": phoneNo,
       "Password": password,
+      "Gender": gender,
+      "ProfileUrl": profileUrl,
     };
   }
 
   // Create an instance from a Firestore document
   factory UserModel.fromMap(Map<String, dynamic> map, {String? id}) {
     return UserModel(
-      id: id,
+      id: id ?? map['id'], // Default to map['id'] if id is null
       name: map['Name'] ?? '',
       phoneNo: map['Phone'] ?? '',
       password: map['Password'] ?? '',
       email: map['Email'] ?? '',
+      gender: map['Gender'] ?? '',
+      profileUrl: map['ProfileUrl'] ?? '',
     );
   }
 }
